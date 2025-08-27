c70e005487ba637b840d5616ba2d5147caf58075
--list <yuri.mandina@gmail.com>
2025-08-26 21:25:51 -0300

    Enhance mobile menu functionality and update navigation links; add 'Sobre' and 'Habilidades' sections to the homepage

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,15 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle('menu-nav-active');
 });
+
+/* Close menu when anchor is clicked */
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-nav-active')) {
+            body.classList.remove('menu-nav-active');
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+});
