088cdcafd2b0ed36477750d6acae024927071d87
--list <yuri.mandina@gmail.com>
2025-08-31 16:37:22 -0300

    Add animations to elements on scroll and update styles for improved visual effects

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,25 @@ navItem.forEach(item => {
         }
     })
 });
+
+/* Animation */
+
+const item = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+    
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
